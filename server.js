c62b21373d799501d99d29fe1c409f29435ff37d
jjanders <jjanders@users.noreply.github.com>
2017-02-06 11:33:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -130,6 +130,6 @@ app.put('/movies/:id', function(req,res){
 	res.redirect('/movies');
 });
 
-app.listen(3000, function(){
+app.listen(3000||process.env.PORT, function(){
 	console.log('listening on port 3000!')
 });
